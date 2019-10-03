9e1f859038dd1ee7f8ba31934bd8986ee1df3fa2
Kinrezc <matt.czernik@gmail.com>
2019-10-03 12:53:21 -0400

    failing logs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import Web3 from 'web3';
 import './App.css';
 
+const web3 = new Web3(Web3.givenProvider);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	return (
+		<div>
+			<button onClick={() => web3.eth.getBlockNumber().then(res => console.log(res))}>
+				Test button
+			</button>
+		</div>
   );
 }
 
